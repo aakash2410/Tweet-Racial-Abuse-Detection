2d6ca0ea9091be6df4a89faf6ace3df7f3423988
aakash2410 <aakash.sangani2410@gmail.com>
2021-12-20 13:20:34 +0530

    new argv added for simplicity and generalization

--- slur_check.py
+++ slur_check.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from copy import copy
-from nltk.tokenize import word_tokenize
+from sys import argv
+
 
 """Some assumptions for this function are that the csv or excel file will have a column named 'tweets' 
 and also, there will be a '/end' string at the end of each tweet in a txt file.
@@ -17,8 +18,7 @@ def read_input(file_name):
 		lines = f.read()
 		lines = lines.split('/end')
 		lines = [tweet.strip() for tweet in lines] #removes white space and newline
-		lines = [tweet.lower() for tweet in lines if tweet] #removes empty strings and lowers the case
-		print(lines)
+		lines = [tweet for tweet in lines if tweet] #removes empty strings and lowers the case
 		df = pd.DataFrame(lines)
 		df['tweets'] = df[0]
 		return df.drop(columns = 0)
@@ -27,19 +27,15 @@ f = open('slur_word_list.txt', 'r')
 slur_word_list = f.readlines()
 slur_word_list = [word.strip() for word in slur_word_list] #removes white space and newline
 slur_word_list = [word.lower() for word in slur_word_list if word] #removes empty strings and lowers the case
-df = read_input('tweets.txt')
+df = read_input(argv[1])
 df['abuse_score'] = list('0'*len(df['tweets']))
-print(df)
+
 for i in range(len(df['tweets'])):
-	abuse_score = 0 
+	abuse_score = 0 			#to calculate abuse score, we +1 whenever a slur is spotted.
 	tweet = copy(df['tweets'][i])
-	tweet_tokenized = word_tokenize(tweet)
-	for tweet_word in tweet_tokenized:
-		if tweet_word in slur_word_list:
+	for slur in slur_word_list:
+		if slur.lower() in tweet.lower():
 			abuse_score +=1
 	df["abuse_score"][i] = abuse_score
 
 df.to_csv('tweet_abuse_scores.csv', index = False)
-
-	
-
