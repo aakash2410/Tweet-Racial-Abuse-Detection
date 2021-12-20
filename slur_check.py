00ef0071c7e379aaf17f01c0299f831595a1ec74
aakash2410 <aakash.sangani2410@gmail.com>
2021-12-20 12:48:11 +0530

    text processing done and abuse scores given

--- slur_check.py
+++ slur_check.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from copy import copy
+from nltk.tokenize import word_tokenize
 
 """Some assumptions for this function are that the csv or excel file will have a column named 'tweets' 
 and also, there will be a '/end' string at the end of each tweet in a txt file.
@@ -14,7 +16,30 @@ def read_input(file_name):
 		f = open(file_name, "r")
 		lines = f.read()
 		lines = lines.split('/end')
+		lines = [tweet.strip() for tweet in lines] #removes white space and newline
+		lines = [tweet.lower() for tweet in lines if tweet] #removes empty strings and lowers the case
+		print(lines)
 		df = pd.DataFrame(lines)
 		df['tweets'] = df[0]
 		return df.drop(columns = 0)
+
+f = open('slur_word_list.txt', 'r')
+slur_word_list = f.readlines()
+slur_word_list = [word.strip() for word in slur_word_list] #removes white space and newline
+slur_word_list = [word.lower() for word in slur_word_list if word] #removes empty strings and lowers the case
+df = read_input('tweets.txt')
+
+
+for i in range(len(df['tweets'])):
+	abuse_score = 0 
+	tweet = copy(df['tweets'][i])
+	tweet_tokenized = word_tokenize(tweet)
+	for tweet_word in tweet_tokenized:
+		if tweet_word in slur_word_list:
+			abuse_score +=1
+	print(abuse_score)
+		
+	
+
+	
 
